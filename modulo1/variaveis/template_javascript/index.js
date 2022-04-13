f57d11ded76c18a665fb4360762297143e78e7ff
Zeneth Nunes de Souza <zenunes250@gmail.com>
2022-04-12 22:03:47 -0300

    Realizei os exercicios referente aula de variaveis

---
+++ modulo1/variaveis/template_javascript/index.js
@@ -0,0 +1,69 @@
+//Resposta exer 1 - 10 e 5
+//Resposta exer 2 - 10/10/20
+/*Resposta exer 3 - Ele está solicitando que o usuario insira no console as informacoes solicitadas e com isso vai imprimir
+no console as respsotas que o usuario informou. Sugestão de nome para variável : let p = prompt("Qual sua carga horário de trabalho por dia?")
+let t = prompt("Qual o valor da sua remuneração diaria?")*/
+
+/*Resposta ex 1 Escrita de código
+
+A - let nome  
+console.log(typeof nome)
+
+B - let idade 
+C - console.log(typeof idade)
+D - Erro do meu console : undefined - Pois estamos declarando uma varialvel sem valor*/
+
+/*E 
+const nome = ("Qual o seu nome")
+const idade = ("Qual sua idade")
+
+console.log(prompt("Qual seu nome"))
+console.log(prompt("Qual sua idade"))
+
+console.log(typeof nome)
+
+console.log(typeof idade)*/ 
+
+//F - Resposta letra E :  String e String//
+
+/*G - Resposta
+const nome = prompt("Meu nome é")
+const idade = prompt("Qual sua idade")
+
+console.log(nome, idade)
+
+console.log("Olá" , nome , "você tem" , idade , "anos")*/
+
+/*Resposta exercicio 2
+
+const */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
