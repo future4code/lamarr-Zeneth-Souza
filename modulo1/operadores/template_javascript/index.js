508c00fb3966eec52140cae893353776e60d97d3
Zeneth Nunes de Souza <zenunes250@gmail.com>
2022-04-16 17:50:25 -0300

    resolução dos exercicios de operadores de logica

---
+++ modulo1/operadores/template_javascript/index.js
@@ -0,0 +1,56 @@
+//Respostas de interpetração de código
+//a - False
+//b - False
+//c - True
+//d - Blooneano
+
+//Resposta exercicio 2
+//O resultado será uma string pois os numeros solicitados ao 
+//usuários estão entre "".
+
+//Resposta exercicio3
+/*let primeiroNumero = prompt("Digite um numero")
+let segundoNumero = prompt("Digite outro numero")
+
+const soma = Number(primeiroNumero) + Number(segundoNumero)
+console.log(soma)*/
+
+//Exercicios de escrita de código
+
+//Exercicio 1
+
+//const idade = Number(prompt("Qual sua idade?"))
+//const idadeAmigo = Number(prompt("Qual a idade do seu melhor amigo?"))
+//const comparaIdades = idade > idadeAmigo;
+//console.log("Sua idade é maior do que a do seu melhor amigo?", comparaIdades);
+
+//Exercicio 2
+
+//const numeroPar = Number(prompt("Digite um numero par!"))
+//console.log("Resto da divisao por 2", Number(numeroPar) / 2)
+
+//O resto da divisão de um numero par por 2 sempre será 0
+//Se inserimos um numero impar o resultado sempre será 1
+
+//Exercicio 3
+
+//const idade = Number(prompt("Digite sua idade"))
+
+//console.log("a. Idade em meses", idade*12)
+//console.log("b. Idade em dias", idade*365)
+//console.log("c. Idade em horas", idade*365*24)
+
+// Exercício 4
+/*const num1 = Number(prompt("Digite um número"))
+const num2 = Number(prompt("Digite outro número"))
+
+console.log("O primeiro numero é maior que segundo?", num1 > num2)
+console.log("O primeiro numero é igual ao segundo?", num1 === num2)
+console.log("O primeiro numero é divisível pelo segundo?", num1 % num2 === 0)
+console.log("O segundo numero é divisível pelo primeiro?", num2 % num1 === 0)*/
+
+
+
+
+
+
