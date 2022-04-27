cd875b84d60f0a02dafe712b0c4721d91bcbf96a
Zeneth Nunes de Souza <zenunes250@gmail.com>
2022-04-26 22:20:07 -0300

    resolução exercicios aula de objetos

---
+++ modulo1/objetos/index.js
@@ -0,0 +1,88 @@
+// Exercicio de interpretação de código
+//1 - 
+//a - Mateus Nachtergaele / Virginia Cavendish / Virginia Cavendish
+ 
+//2 - 
+//a) Juca, idade 3, raca, SRD / Juba, idade 3 , raca SRD / Jubo idade 3, raca SRD
+//b) Ela copia o objeto acima e para que não precise haver muita repetição e você consegue alterar o objeto.
+
+//3
+//a) Vai ser impresso no console false, pois no objeto já está declarado que caio não é beckender e underfined pois altura não está declarada no objeto.
+
+//Exercicio de escrita de código
+
+//1 - 
+
+//a)
+
+/*const pessoa = {
+    nome: "Bilbo Bolseiro",
+    apelidos: ["Pé Peludo", "Ladrão" , "Bolseiro"]
+}
+console.log(`Eu sou ${pessoa.nome}, mas pode me chamar de: ${pessoa.apelidos[0]}, ${pessoa.apelidos[1]}, ${pessoa.apelidos[2]}`)
+
+//b)
+
+const pessoa1={...pessoa, apelidos: ["careca" , "baixinho" , "perna torta"]
+}
+console.log(`Eu sou ${pessoa1.nome}, mas pode me chamar de: ${pessoa1.apelidos[0]}, ${pessoa1.apelidos[1]}, ${pessoa1.apelidos[2]}`)*/
+
+//2
+
+//a)
+ 
+/*const pessoa1 = {
+    nome: "Luciane",
+    idade: 47, 
+    profissão: "Contadora"
+}
+
+const pessoa2 = {
+    nome: "Zeneth",
+    idade: 34, 
+    profissão: "Estudante"
+}
+
+function retornaInformação (objeto) {
+
+    let arrayInformações = [objeto.nome, objeto.nome.lenght, objeto.idade, objeto.profissão, objeto.profissão.length ]
+    return arrayInformações
+}
+   console.log(retornaInformação(pessoa1))
+   console.log(retornaInformação(pessoa2))*/
+
+//3   
+
+//a)
+
+/*let carrinho = []
+ 
+const fruta1 =  {
+    nome: "Pitaya",
+    disponibilidade: true,
+
+}
+
+const fruta2 =  {
+    nome: "Morango",
+    disponibilidade: true,
+
+}
+const fruta3 =  {
+    nome: "Banana",
+    disponibilidade: true,
+
+}
+
+console.log()
+
+function feira(fruta){
+    carrinho.push(fruta)
+}
+feira(fruta1)
+feira(fruta2)
+feira(fruta3)
+
+console.log(carrinho)*/
+
+
