e12f4b7c69c580b8ad3e0433ce7980fbbe7172f8
Zeneth Nunes de Souza <zenunes250@gmail.com>
2022-07-14 20:57:14 -0300

    Estapas Mockdados

---
+++ modulo3/thinking-react/MockDeDados.js
@@ -0,0 +1,46 @@
+const dadosProduto1=[
+    {id:"1",
+    name:"Caneca personalizada1",
+    preço:"59,90"
+    photo:'https://shopee.com.br/Canecas-Personalizadas-Arte-Variadas-i.336227777.8371111922'
+}
+]
+const dadosProduto2=[
+    {id:"1",
+    name:"Caneca personalizada2",
+    preço:"59,90"
+    photo:'https://www.extra.com.br/canecas-personalizadas-de-aniversario/p/1514234797'
+}
+]
+
+const dadosProduto3=[
+    {id:"1",
+    name:"Caneca personalisada3 ",
+    preço:"59,90"
+    photo:'https://www.shoptime.com.br/produto/2074766471'
+}
+]
+
+const dadosProduto4=[
+    {id:"1",
+    name:"Caneca personalisada",
+    preço:"59,90"
+    photo:'https://canecariazelele.com.br/produtos/caneca-personalizada-foto-do-seu-pet/'
+}
+]
+
+const dadosProduto5=[
+    {id:"1",
+    name:"Caneca personalisada5",
+    preço:"59,90"
+    photo:'https://megustapresentes.com/products/caneca-personalizada-com-frase-e-qrcode-do-spotify'
+}
+]
+
+const dadosProduto6=[
+    {id:"1",
+    name:"Caneca personalisada",
+    preço:"59,90"
+    photo:'https://produto.mercadolivre.com.br/MLB-786563192-caneca-personalizada-sua-ideia-aqui-_JM'
+}
+]
